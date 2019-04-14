c1f27cce85a655aa12c64761cbcd7d4d110153f5
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-04-14 13:56:55 -0500

    sun 4.18

--- server.js
+++ server.js
@@ -107,7 +107,8 @@ app.get("/articles", function (req, res) {
   db.Article.find({})
     .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.render(dbArticle);
+      //res.json(dbArticle);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
